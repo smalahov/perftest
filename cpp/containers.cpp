c6521dffc74ef0ac8804062a38e0952360fbc643
smalahov <malahovs84@gmail.com>
2025-07-01 18:02:10 +0300

    Copy optimization for trivial types tests

--- cpp/containers.cpp
+++ cpp/containers.cpp
@@ -46,6 +46,8 @@ void BM_ChangeMapKey_RemoveInsert(benchmark::State& state) {
         }
     }
 }
+BENCHMARK(BM_ChangeMapKey_RemoveInsert)->Arg(1000);
+
 void BM_ChangeMapKey_ExtractNode(benchmark::State& state) {
     std::unordered_map<int, std::pair<int, int>> map;
 
@@ -64,5 +66,53 @@ void BM_ChangeMapKey_ExtractNode(benchmark::State& state) {
         }
     }
 }
-BENCHMARK(BM_ChangeMapKey_RemoveInsert)->Arg(1000);
 BENCHMARK(BM_ChangeMapKey_ExtractNode)->Arg(1000);
+
+struct TData {
+    long long A;
+    char B;
+    double C;
+    int D[12];
+};
+
+struct TTrivial {
+    // Just to make Trivial equal in size compared to Virtual
+    void* FakeVMT;
+    TData Data;
+};
+
+struct TVirtual {
+    TData Data;
+    virtual ~TVirtual() = default;
+};
+
+static_assert(sizeof(TTrivial) == sizeof(TVirtual));
+static_assert(std::is_trivial_v<TTrivial> && std::is_trivially_copyable_v<TTrivial>);
+static_assert(!std::is_trivial_v<TVirtual> && !std::is_trivially_copyable_v<TVirtual>);
+
+// Test explanation:
+// vector copy constructor uses __uninitialized_copy which uses
+// std::copy to manipulate actual elements data. std::copy uses memcpy/memmove for trivial objects
+// which is supposed to be faster
+template<typename T>
+void BM_VectorCopy(benchmark::State& state) {
+    std::vector<T> src(state.range(0));
+    for (auto _ : state) {
+        std::vector<T> v;
+        benchmark::DoNotOptimize(v = src);
+    }
+}
+BENCHMARK(BM_VectorCopy<TTrivial>)->Arg(1000);
+BENCHMARK(BM_VectorCopy<TVirtual>)->Arg(1000);
+
+template<typename T>
+void BM_VectorPushBack(benchmark::State& state) {
+    for (auto _ : state) {
+        std::vector<T> v;
+        while(v.size() < state.range(0)) {
+            v.emplace_back();
+        }
+    }
+}
+BENCHMARK(BM_VectorPushBack<TTrivial>)->Arg(1000000);
+BENCHMARK(BM_VectorPushBack<TVirtual>)->Arg(1000000);
